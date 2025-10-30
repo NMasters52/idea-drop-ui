8875cdcc191dfe34afce6cabf058e1c74568cb43
nmasters <nmasters52@gmail.com>
2025-10-29 20:01:01 -0400

    add 404 not found layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,10 +2,14 @@ import {
   HeadContent,
   Outlet,
   createRootRouteWithContext,
+  Link,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { QueryClient } from "@tanstack/react-query";
+
+//components
+import Header from "@/components/Header";
 
 type RouterContext = {
   queryClient: QueryClient;
@@ -24,10 +28,21 @@ export const Route = createRootRouteWithContext<RouterContext>()({
       },
     ],
   }),
-  component: () => (
-    <>
+  component: () => RootLayout(),
+  notFoundComponent: () => NotFound(),
+});
+
+const RootLayout = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col ">
       <HeadContent />
-      <Outlet />
+      <Header />
+      <main className="flex justify-center p-6">
+        <div className="w-full max-w-4xl bg-white rounded-2xl shadow-lg p-8">
+          <Outlet />
+        </div>
+      </main>
+
       <TanStackDevtools
         config={{
           position: "bottom-right",
@@ -39,6 +54,23 @@ export const Route = createRootRouteWithContext<RouterContext>()({
           },
         ]}
       />
-    </>
-  ),
-});
+    </div>
+  );
+};
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center py-20">
+      <h1 className="text -4xl font-bold text-gray-800 mb-4">404</h1>
+      <p className="text-lg text-gray-600 mb-4">
+        Ooops the page you're looking for does not exist
+      </p>
+      <Link
+        className="px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+        to="/"
+      >
+        Go Back Home
+      </Link>
+    </div>
+  );
+};
